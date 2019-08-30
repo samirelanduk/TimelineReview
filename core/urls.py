91a6d79240c4a9af0d2c08233879772aeb324480
Sam Ireland <sam.ireland.uk@gmail.com>
2019-08-30 22:17:36 +0100

    Add link to PDFs

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.conf import settings
+from django.conf.urls.static import static
 from core.views import *
 
 urlpatterns = [
  path("", home)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

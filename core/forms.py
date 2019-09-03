f5c62184d12494b73419b7580cf209b116f85ebf
Sam Ireland <sam.ireland.uk@gmail.com>
2019-09-03 12:19:50 +0100

    Add tag saving

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,15 @@ class PaperForm(forms.ModelForm):
     class Meta:
         model = Paper
         exclude = []
+    
+    tags = forms.CharField(required=False)
+
+    def save(self, *args, **kwargs):
+        model = forms.ModelForm.save(self, *args, **kwargs)
+        tags = [t.strip() for t in self.cleaned_data["tags"].split(",")]
+        for tag in tags:
+            try:
+                tag_object = Tag.objects.get(name=tag)
+            except: tag_object = Tag.objects.create(name=tag)
+            model.tags.add(tag_object)
+        model.save()
